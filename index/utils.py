9713508baaec781dcc5e06c06e79033874b2fbcb
Sergey <sergejgrigorev29@gmail.com>
2025-04-12 16:29:32 +0800

    Add sql table 'weather_SRC'

--- index/utils.py
+++ index/utils.py
@@ -1,1 +1,17 @@
 WEATHER_ACCESS_KEY = '4e7d837b-8581-4616-871c-e553e993e308'
+
+# HOST = 'localhost'
+# USERNAME = 'postgres'
+# PSWD = '1234'
+# DB_NAME = 'weather_stat'
+#
+# BOT_TOKEN = "8051715081:AAENgge1BIc2xfPs9UHVd37Q_DpcRqvYGfo"
+# CHAT_ID = "6414160592"
+
+HOST = ''
+USERNAME = ''
+PSWD = ''
+DB_NAME = ''
+
+BOT_TOKEN = ""
+CHAT_ID = ""
